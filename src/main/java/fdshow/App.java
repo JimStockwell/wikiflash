1ccfdc192b1318e5681fcb5847239becf2855ed3
Jim Stockwell <jimstockwellatwork@yahoo.com>
2020-06-22 08:45:12 -0700

    Neatens file references.
    Changes both file references to relative references.
    Moves "All.txt" to the project directory.

--- src/main/java/fdshow/App.java
+++ src/main/java/fdshow/App.java
@@ -4,13 +4,18 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
 
+/**
+ * Maintains consistency between a flashcard program and a wiki.
+ *
+ * Usage: App flashcardFile wikiFile
+ */
 public class App 
 {
     public static void main( String[] args )
     throws java.io.FileNotFoundException
     {
         FDFileData fileData = new FDFileData();
-        fileData.loadFrom(new BufferedReader(new FileReader("/Users/jimstockwell/Downloads/All.txt")));
-        fileData.saveTo(new FileOutputStream("/Users/jimstockwell/fdshow/AllRewrite.txt"));
+        fileData.loadFrom(new BufferedReader( new FileReader("All.txt") ) );
+        fileData.saveTo(new FileOutputStream("AllRewrite.txt") );
     }
 }
